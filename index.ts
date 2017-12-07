9c0138220f7d5a4ee50963c66257c015a595538d
yangjiadong <yangjiadong@ycode.cn>
2017-12-07 23:11:05 +0800

    [init]learnLet:rocket:

---
+++ index.ts
@@ -0,0 +1,21 @@
+function Hello(name=""){
+    if(name==="Jiadong"){
+        greet="fuck yourself";
+    }else if(name==="YHY"){
+        greet="you are cute";
+    }   
+    console.log(greet);
+    var greet;
+}
+
+Hello("Jiadong");
+var a=1;
+var b=2;
+if(a===1){
+    var a=10;
+    let b=10;
+    console.log(a);
+    console.log(b);
+}
+console.log(a)
+console.log(b);
---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+function Hello(name) {
+    if (name === void 0) { name = ""; }
+    if (name === "Jiadong") {
+        greet = "fuck yourself";
+    }
+    else if (name === "YHY") {
+        greet = "you are cute";
+    }
+    console.log(greet);
+    var greet;
+}
+Hello("Jiadong");
+var a = 1;
+var b = 2;
+if (a === 1) {
+    var a = 10;
+    var b_1 = 10;
+    console.log(a);
+    console.log(b_1);
+}
+console.log(a);
+console.log(b);
